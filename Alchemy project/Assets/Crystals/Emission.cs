d2af185c90907346fb24cb495bbec1e54eeb17ae
Steven Taylor <ST1174@live.mdx.ac.uk>
2020-03-04 01:15:16 +0000

    First Commit for alch-Mix Scene. ST

---
+++ Alchemy project/Assets/Crystals/Emission.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Renderer))]
+
+public class Emission : MonoBehaviour
+{
+
+    new Renderer renderer;
+    Color emissionColor;
+    Material material;
+
+    private float Cooking;
+    private readonly bool IsCooked = false;
+
+    public GameObject crystalLight;
+    private bool emissionOn = false;
+    private float rangeAmount;
+    private float fluxAmount = 0.001f;
+    
+   
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        material = renderer.material;
+        emissionColor = material.GetColor("_EmissionColor");
+
+
+        crystalLight.GetComponent<Light>().range = 0.12f;
+        rangeAmount = crystalLight.GetComponent<Light>().range;
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))  // Change to on-trigger stay for game
+        {
+            Cooking += Time.deltaTime;
+            print(Cooking);
+
+            if (Cooking >= 10)  
+            {
+                Activate(true, 1f);
+                TurnOnLight();
+            }
+        }
+        else
+        {
+            Cooking = 0;
+        }
+
+        if (emissionOn == true)
+        {
+
+            rangeAmount += fluxAmount;
+
+            crystalLight.GetComponent<Light>().range = rangeAmount;
+
+            if (rangeAmount >= 0.16f)
+            
+                fluxAmount = -fluxAmount; // One way to change positve to negative number (works other way round)
+            
+
+            if (rangeAmount <= 0.12f)
+            
+                fluxAmount  *= -1; // another way to change number from negative to positive.
+            
+        }
+ 
+    }
+
+    public void TurnOnLight()           //Function for turning on light source on crystals.
+    {
+        crystalLight.SetActive(true); 
+    }
+    
+
+    public void Activate(bool on, float intensity)             //Access the Emissive material and enables/disables it.
+
+    {
+        if (on)
+        {
+            emissionOn = true;
+            material.EnableKeyword("_EMISSION");
+            material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+
+            material.SetColor("_EmissionColor", emissionColor * intensity);
+        }
+
+        else
+        { 
+            material.DisableKeyword("_EMISSION");
+            material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
+
+            material.SetColor("_EmissionColor", Color.black);
+        }
+
+    }
+
+}
---
+++ Alchemy project/Assets/DayNightCycle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNightCycle : MonoBehaviour
+{
+
+    private float daycycle;
+    private const float Seconds_Per_Day = 360f;
+    private Text timeText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText = GameObject.Find("timeText").GetComponent<Text>(); // text search
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        daycycle += Time.deltaTime / Seconds_Per_Day; //in game time vs real time 
+
+        float daycycleNormalised = daycycle % 1f;
+        float HoursPerDay = 24f; // 24 hours
+
+        string hoursString = Mathf.Floor(daycycleNormalised * 24f).ToString("00"); // Hours calculation out of 24 hours
+
+        float minutesPerHour = 60f; // 60 mins per hour
+        string minutesString = Mathf.Floor(((daycycleNormalised * HoursPerDay) % 1f) * minutesPerHour).ToString("00");  //minutes calculation (not, rounded up)
+
+        timeText.text = hoursString + ":" + minutesString;
+    }
+}
